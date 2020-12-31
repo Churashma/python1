813f9e253c07128069fded6f5fb06e2facecdf77
Churashma <53474187+Churashma@users.noreply.github.com>
2020-12-31 20:56:40 +0530

    Update create.py

--- create.py
+++ create.py
@@ -1,9 +1,29 @@
-import random
-li = ["02-12-1999","05-20-2020","11-15-2006","04-03-2009","09-09-2000","12-22-2020","12-09-2000","03-30-2000","05-29-2020","10-10-1999"]
+import xlwt
+from xlwt import Workbook
 
+wb = Workbook() 
 
-for i in range(10):
-    ele = random.choice(li)
-    f = open(ele+".txt",'w')
-    li.remove(ele)
-    f.close()
+sheet1 = wb.add_sheet('Sheet 1') 
+
+sheet1.write(0,0,'USN')
+sheet1.write(0,1,'Name')
+sheet1.write(0,2,'Marks')
+sheet1.write(1,0,'121')
+sheet1.write(1,1,'Chris')
+sheet1.write(1,2,'100')
+sheet1.write(2,0,'122')
+sheet1.write(2,1,'Erik')
+sheet1.write(2,2,'99')
+sheet1.write(3,0,'123')
+sheet1.write(3,1,'Evans')
+sheet1.write(3,2,'89')
+sheet1.write(4,0,'124')
+sheet1.write(4,1,'Laura')
+sheet1.write(4,2,'100')
+sheet1.write(5,0,'125')
+sheet1.write(5,1,'Sheltear')
+sheet1.write(5,2,'69')
+
+wb.save('xlwt example.xls') 
+print("Excel file created")
+©
